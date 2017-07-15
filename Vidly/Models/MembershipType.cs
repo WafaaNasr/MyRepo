332f26489e1ef005d729ebe7022bd496e53bf246
Wafaa <Wafaa.AANasr@gmail.com>
2017-07-15 18:01:23 +0200

    Adding Name Property for MembershipType + Seeding it

--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -3,6 +3,8 @@
     public class MembershipType
     {
         public byte Id { get; set; }
+
+        public string Name { get; set; }
         public short SignUpFree { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
