26bfd7e6724f004f24ee103badece6cd4376ffa5
Wafaa <Wafaa.AANasr@gmail.com>
2017-07-15 17:41:42 +0200

    Getting Customers from Db Table, Revamp table UI

--- Vidly/Controllers/CustomerController.cs
+++ Vidly/Controllers/CustomerController.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
 
@@ -7,21 +6,39 @@ namespace Vidly.Controllers
 {
     public class CustomerController : Controller
     {
-        private readonly List<Customer> _customers = new List<Customer>
-            {
-                new Customer {Id=1,Name="John Smith" },
-                new Customer {Id=2,Name="Mary Williams" }
-            };
+        #region Initialize&&Dispose
+
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public CustomerController()
+        {
+            _applicationDbContext = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _applicationDbContext.Dispose();
+        }
+
+        #endregion Initialize&&Dispose
+
+        #region ControllerActions
+
         public ActionResult Index()
         {
-
-            return View(_customers);
+            //EF as "Deferred Executions" won't execute this line until one uses the Customers or iterates on them
+            var customers = _applicationDbContext.Customers;
+            //ToList() will force the EF execute the query immediately
+            return View(customers.ToList());
         }
+
         public ActionResult Detail(int id)
         {
             if (id != 1 && id != 2)
                 return HttpNotFound();
-            return View(_customers.FirstOrDefault(c => c.Id == id));
+            return View(_applicationDbContext.Customers.FirstOrDefault(c => c.Id == id));
         }
+
+        #endregion ControllerActions
     }
 }
