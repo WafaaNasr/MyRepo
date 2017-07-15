912b7e81de520f033cd4978dd1f3f120830cb8f6
Wafaa <Wafaa.AANasr@gmail.com>
2017-07-15 17:55:09 +0200

    Allow Eager Loading for MembershipType in Customers.cs

--- Vidly/Controllers/CustomerController.cs
+++ Vidly/Controllers/CustomerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
@@ -27,16 +28,17 @@ namespace Vidly.Controllers
         public ActionResult Index()
         {
             //EF as "Deferred Executions" won't execute this line until one uses the Customers or iterates on them
-            var customers = _applicationDbContext.Customers;
+            var customers = _applicationDbContext.Customers.Include(c=>c.MembershipType);// For Supporting Eager loadind for the Navigation Properties 
             //ToList() will force the EF execute the query immediately
             return View(customers.ToList());
         }
 
         public ActionResult Detail(int id)
         {
-            if (id != 1 && id != 2)
+            var customer = _applicationDbContext.Customers.FirstOrDefault(c => c.Id == id);
+            if (customer == null)
                 return HttpNotFound();
-            return View(_applicationDbContext.Customers.FirstOrDefault(c => c.Id == id));
+            return View(customer);
         }
 
         #endregion ControllerActions
