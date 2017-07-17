168a3039fb58dc6d017a2b65000d2f7d55bafe1b
Wafaa <Wafaa.AANasr@gmail.com>
2017-07-17 14:30:35 +0200

    Adding Movie Creation and Edit Forms

--- Vidly/Controllers/MovieController.cs
+++ Vidly/Controllers/MovieController.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
 using System.Data.Entity;
+using System.Data.Entity.Validation;
 
 namespace Vidly.Controllers
 {
@@ -52,6 +54,45 @@ namespace Vidly.Controllers
         {
             var movie = _applicationDbContext.Movies.Include(m => m.Genre).FirstOrDefault(m => m.Id == id);
             return movie == null ? (ActionResult)HttpNotFound() : View(movie);
+        }
+
+        public ActionResult Create()
+        {
+            var movieViewModel = new MovieViewModel { Genres = _applicationDbContext.Genre.ToList(), Movie = null };
+            return View("MovieForm", movieViewModel);
+        }
+        public ActionResult Edit(int id)
+        {
+            var movieViewModel = new MovieViewModel { Genres = _applicationDbContext.Genre.ToList(), Movie = _applicationDbContext.Movies.Single(m => m.Id == id) };
+
+            return View("MovieForm", movieViewModel);
+        }
+
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _applicationDbContext.Movies.Add(movie);
+            }
+            else
+            {
+                var movieIndDb = _applicationDbContext.Movies.Single(m => m.Id == movie.Id);
+                movieIndDb.GenreId = movie.GenreId;
+                movieIndDb.Name = movie.Name;
+                movieIndDb.ReleaseDate = movie.ReleaseDate;
+                movieIndDb.NumberInStock = movie.NumberInStock;
+            }
+            try
+            {
+                _applicationDbContext.SaveChanges();
+            }
+            catch (DbEntityValidationException dbEntityValidation)
+            {
+                Console.Write(dbEntityValidation.Message);
+                throw dbEntityValidation;
+            }
+            return RedirectToAction("Index");
         }
         #endregion
     }
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,18 +1,29 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public DateTime ReleaseDate { get; set; }
+
+        [Required]
         public DateTime DateAdded { get; set; }
 
+        [Required]
+        [Display(Name = "Number in Stock")]
         public int NumberInStock { get; set; }
 
         public Genre Genre { get; set; }
+
+        [Required]
+        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
     }
 }
